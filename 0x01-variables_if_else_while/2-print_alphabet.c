84dd3ad025e7b2065a1af454bc21fd8096a08816
desiyalew <dessiemagna@gmail.com>
2022-04-08 00:22:48 -0700

    2. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * this is the 3rd c program 
+ *
+ */
+int main(void)
+{
+char low;
+for (low ='a'; low <= 'z';low++)
+	putchar(low)
+	print("\n");
+return (0);
+}
