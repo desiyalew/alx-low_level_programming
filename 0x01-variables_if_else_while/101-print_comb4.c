fb612f410f9f3be637d588db651748cdd81223d8
desiyalew <dessiemagna@gmail.com>
2022-04-08 10:42:37 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,9 +12,9 @@ int j;
 int k;
 for (i = 48; i < 56; i++)
 {
-for (j = i + 1;j < 57;j++)
+for (j = i + 1; j < 57; j++)
 {
-for (k = j + 1;k < 58;k++)
+for (k = j + 1; k < 58; k++)
 {
 putchar(i);
 putchar(j);
