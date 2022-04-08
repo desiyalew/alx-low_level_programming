9ab9430c077bc207bfce53130c636af60e54879f
desiyalew <dessiemagna@gmail.com>
2022-04-08 10:02:35 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,10 +7,10 @@
  **/
 int main(void)
 {
-	char low;
+char low;
 
-	for (low = 'a'; low <= 'z'; low++)
-		putchar(low);
-	putchar('\n');
-	return (0);
+for (low = 'a'; low <= 'z'; low++)
+putchar(low);
+putchar('\n');
+return (0);
 }
