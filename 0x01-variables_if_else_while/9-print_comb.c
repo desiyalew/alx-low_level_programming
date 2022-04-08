d8939a388382c43d2d4a53906cb1b03a1cbd6f85
desiyalew <dessiemagna@gmail.com>
2022-04-08 08:38:50 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
  * main - entry point 
- *
+ * 
  * Return: Always 0 (Success)
- */
+ **/
 int main(void)
 {
 	int d;
@@ -18,7 +17,8 @@ int main(void)
 		{
 			putchar(',');
 			putchar(' ');
-		}		
+		}
+
 	}
 	putchar('\n');
 
