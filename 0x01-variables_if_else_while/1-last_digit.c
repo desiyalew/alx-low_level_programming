35e06460c55b2fd2e3fd4b4befd6d4ae05ac9127
desiyalew <dessiemagna@gmail.com>
2022-04-08 09:32:45 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,7 +19,8 @@ if (n % 10 >5)
 printf("last digit of %i is %i and is greater than 5\n", n, n % 10);
 }
 else if (n % 10 == 0)
-{printf("Last digit of %i is %i and is 0\n", n, n % 10);
+{
+printf("Last digit of %i is %i and is 0\n", n, n % 10);
 }
 else
 {
