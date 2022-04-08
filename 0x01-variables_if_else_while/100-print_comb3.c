40823bf05ceb5962259ee7ae08fd57dc7dc6a7e2
desiyalew <dessiemagna@gmail.com>
2022-04-08 10:29:11 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,26 +2,26 @@
 #include <stdlib.h>
 #include <time.h>
 /**
+ * main - a simple program that outputs 0-9 separated by commas
  *
- *
- *10th alx c programming
+ * Return: 0 on success
  */
 int main(void)
 {
-	int d;
-	for (d = 0; d < 90; d++)
-	{
-		for ()
-		{
-			putchar((d / 10) + '0');
-			putchar((d % 10) + '0');
-                }
-		if (d != 89)
-		{
-			putchar(','); 
-			putchar(' ');
-		}
-	}
-	putchar('\n');
-	return (0);
+int d;
+for (d = 0; d < 90; d++)
+{
+for ()
+{
+putchar((d / 10) + '0');
+putchar((d % 10) + '0');
+}
+if (d != 89)
+{
+putchar(','); 
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
 }
