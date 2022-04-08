4336a2d46d4f3a646fd767359c77d977dd0edcdc
desiyalew <dessiemagna@gmail.com>
2022-04-08 10:25:21 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -2,19 +2,19 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * 
+ * main - a simple program that outputs the hexadecimal digits
  *
- * 9th alx c program
+ * Return: 0 on success
  */
 int main(void)
 {
-	int d;
-	char low;
-	
-	for (d = '0'; d <= '9'; d++)
-		putchar(d);
-	for (low = 'a'; low <= 'f'; low++)
-		putchar(low);
-	putchar('\n');
-	return (0);
+int d;
+char low;
+
+for (d = '0'; d <= '9'; d++)
+putchar(d);
+for (low = 'a'; low <= 'f'; low++)
+putchar(low);
+putchar('\n');
+return (0);
 }
