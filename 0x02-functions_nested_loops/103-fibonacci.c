2a768558658722305f3fd4c7bc1a6265f8605c5b
desiyalew <dessiemagna@gmail.com>
2022-04-12 14:27:14 -0700

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,44 +1,24 @@
 #include <stdio.h>
 /**
- *  * print_fib - prints the first 50 Fibonacci numbers,
- *   * starting with 1 and 2, followed by a new line.
- *    * @len: the length of with the fib sequance should print to.
- *     *
- *      * Return: void
- *       */
-void print_fib(int len)
-{
-	        unsigned int target, i;
-
-		        unsigned int former_l, former_r;
-			        unsigned int latter_l, latter_r;
-				        unsigned int fib_l, fib_r;
-
-					        f = 1;
-						        l = 2;
-							        target = len - 2;
-								        for (i = 0 ; i < target ; i++)
-										        {
-												                fib = f + l;
-														                printf("%d : ", i + 4);
-																                printf("%.0Lf" , fib);
-																		                f = l;
-																				                l = fib;
-																						                if (i < target - 1)
-																									                        printf("\n");
-																								        }
-									        putchar('\n');
-}
-
-
-/**
- *  * main - Entry point,
- *   *
- *    *
- *     * Return: Always 0 (sucess)
- *      */
+ * main - sum even fibonacci numbers under 4 million
+ * Return: Nothing
+ */
 int main(void)
 {
-	        print_fib(98);
-		        return (0);
+	unsigned long count, i, j, k, sums;
 
+	i = sums = 0;
+	j = 1;
+	for (count = 0; count < 50; count++)
+	{
+		k = i + j;
+		i = j;
+		j = k;
+		if (k % 2 == && k < 4000000)
+		{
+			sums += k;
+		}
+	}
+	printf("%d\n", sums);
+	return (0);
+}
