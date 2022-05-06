6b81134fb92d2031e787aba35edc86f900cbf0c7
desiyalew <dessiemagna@gmail.com>
2022-05-06 11:42:13 -0700

    commit for task 0x0C-more_malloc_free

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,91 +1,143 @@
+#include <stdlib.h>
 #include "main.h"
 
 /**
- * _puts - prints a string, followed by a new line,
- * @str: pointer to the string to print
- * Return: void
-*/
-
-
-void _puts(char *str)
+ * _prntstr - prints a string
+ *
+ * @s: string to print
+ */
+void _prntstr(char *s)
 {
-int i = 0;
-while (str[i])
-{
-	_putchar(str[i]);
-	i++;
-}
-
+	while (*s)
+		_putchar(*s++);
 }
 
 /**
- * _atoi - convert a string to an integer.
- * @s: char type string
- * Return: integer converted
+ * numstrchk - checks arg array to see if the are numeric strings, converts
+ * from ascii to byte int, and returns their length. Segfault on null pointer.
+ *
+ * @s: string to check
+ *
+ * Return: Length of string. Exit 98 if not numeric.
  */
-
-int _atoi(const char *s)
+long int numstrchk(char *s)
 {
-    int sign = 1;
-	unsigned long int resp = 0, firstNum, i;
+	long int len = 0;
 
-	for (firstNum = 0; !(s[firstNum] >= 48 && s[firstNum] <= 57); firstNum++)
+	if (*s == 0)
 	{
-		if (s[firstNum] == '-')
+		_prntstr("Error\n");
+		exit(98);
+	}
+
+	while (*s)
+	{
+		if (*s < '0' || *s > '9')
 		{
-			sign *= -1;
+			_prntstr("Error\n");
+			exit(98);
 		}
+		*s -= '0';
+		len++;
+		s++;
 	}
+	return (len);
+}
 
-	for (i = firstNum; s[i] >= 48 && s[i] <= 57; i++)
+/**
+ * _calloc_buffer - allocate a block of memory of size * num and init to '0'
+ *
+ * @num: number of elements to allocate
+ * @size: size of element
+ *
+ * Return: pointer to allocated space, exit 98 on failure
+ */
+void *_calloc_buffer(long int num, long int size)
+{
+	void *ret;
+	char *ptr;
+
+	ret = malloc(num * size);
+	if (ret == 0)
 	{
-		resp *= 10;
-		resp += (s[i] - 48);
+		exit(98);
 	}
 
-	return (sign * resp);
+	size = size * num;
+	ptr = ret;
+	ptr[--size] = 0;
+	while (size--)
+		ptr[size] = '0';
+
+	return (ret);
 }
 
 /**
- * print_int - prints an integer.
- * @n: int
- * Return: 0
+ * trimzero - moves pointer position to after last leading 0 in a string,
+ * or last zero if all zeros
+ *
+ * @s: char * we want to move
+ *
+ * Return: new position
  */
-
-void print_int(unsigned long int n)
+char *trimzero(char *s)
 {
-
-unsigned  long int divisor = 1, i, resp;
-
-for (i = 0; n / divisor > 9; i++, divisor *= 10)
-;
-
-for (; divisor >= 1; n %= divisor, divisor /= 10)
-{
-	resp = n / divisor;
-	_putchar('0' + resp);
-}
-
+	while (*s == '0')
+		if (*(s + 1) != 0)
+			s++;
+		else
+			break;
+	return (s);
 }
 
 /**
- * main - print the result of the multiplication, followed by a new line
- * @argc: int
- * @argv: list
- * Return: 0
+ * main - multiply two  positive integer strings of arbitrary size
+ *
+ * @ac: number of arguments
+ * @av: arugments
+ *
+ * Return: 0 if successful, 98 if failure
  */
-
-int main(int argc, char const *argv[])
+int main(int ac, char **av)
 {
-(void)argc;
+	long int len1, len2, lenres, i, j;
+	char *res;
 
-if (argc != 3)
-{
-	_puts("Error ");
-	exit(98);
-}
-print_int(_atoi(argv[1]) * _atoi(argv[2]));
-_putchar('\n');
+	if (ac != 3)
+	{
+		_prntstr("Error\n");
+		return (98);
+	}
+	av[2] = trimzero(av[2]);
+	av[1] = trimzero(av[1]);
+	if (*av[1] == '0' || *av[2] == '0')
+	{
+		_prntstr("0\n");
+		return (0);
+	}
+	len1 = numstrchk(av[1]);
+	len2 = numstrchk(av[2]);
+	lenres = len1 + len2;
+	res = _calloc_buffer(lenres + 1, sizeof(char));
 
-return (0);
+	for (i = lenres - 1, len1--; len1 >= 0; len1--, i += len2 - 1)
+		for (j = len2 - 1; j >= 0; j--, i--)
+		{
+			res[i] = (av[1][len1] * av[2][j] % 10) + res[i];
+			res[i - 1] = (av[1][len1] * av[2][j] / 10) + res[i - 1];
+			if (res[i] > '9')
+			{
+				res[i] -= 10;
+				res[i - 1]++;
+			}
+		}
+
+	if (*res == '0')
+		_prntstr(res + 1);
+	else
+		_prntstr(res);
+	_putchar('\n');
+	free(res);
+
+	return (0);
 }
