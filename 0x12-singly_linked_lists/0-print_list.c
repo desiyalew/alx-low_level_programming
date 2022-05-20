d4ede8ce7667d8299cb20410ba47d3aec3e2befc
desiyalew <dessiemagna@gmail.com>
2022-05-20 06:45:09 -0700

    this is commit for single linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,5 +1,17 @@
+/*
+ * File: 0-print_list.c
+ */
+
 #include "lists.h"
 #include <stdio.h>
+
+/**
+ * print_list - Prints all the elements of a list_t list.
+ * @h: The list_t list.
+ *
+ * Return: The number of nodes in h.
+ */
+
 size_t print_list(const list_t *h)
 {
 	size_t nodes = 0;
--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,5 +1,14 @@
+/*
+ * File: 4-free_list.c
+ */
+
 #include "lists.h"
 #include <stdlib.h>
+
+/**
+ * free_list - Frees a list_t list.
+ * @head: A pointer to the list_t list.
+ */
 void free_list(list_t *head)
 {
 	list_t *tmp;
