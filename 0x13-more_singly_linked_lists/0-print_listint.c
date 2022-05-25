ba73fb68edf6202c8bb771a29484544d0ad21d20
desiyalew <dessiemagna@gmail.com>
2022-05-24 19:42:42 -0700

    file commited

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -13,14 +13,16 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t nodes = 0;
+	size_t n_nodes = 0;
+
+	if (!h)
+		return (0);
 
 	while (h)
 	{
-		nodes++;
 		printf("%d\n", h->n);
 		h = h->next;
+		n_nodes++;
 	}
-
-	return (nodes);
+	return (n_nodes);
 }
